d7815b73cd4cdf1418aff47e9bfc04676777b583
Natalia <alamo.natalia@gmail.com>
2024-12-06 16:01:28 +0100

    Tailwind installed

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
+import tailwind from '@astrojs/tailwind';
+
 // https://astro.build/config
 export default defineConfig({
-    server: {
-        open: true,
-    }
+  server: {
+      open: true,
+  },
+
+  integrations: [tailwind()]
 });
