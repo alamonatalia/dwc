21d90608043f3b60c674ed6e101fb09fc462fac8
Natalia <alamo.natalia@gmail.com>
2024-12-21 20:45:29 +0100

    working on website

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,6 +1,9 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	content: [
+		'./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}',
+		'./node_modules/clsx/**/*.js'
+	],
 	theme: {
 		extend: {
 			// Design Token Components
@@ -14,6 +17,11 @@ export default {
 			},
 			backgroundImage: { 
 				'hero': 'url("../assets/images/hero.jpg")',
+				'kitchen': 'url("../assets/images/kitchen-1.jpg")',
+				'bathroom': 'url("../assets/images/bathroom.jpg")',
+				'kitchen-2': 'url("../assets/images/kitchen-2.jpg")',
+				'top': 'url("../assets/images/top.jpg")',
+				'marmol': 'url("../assets/images/marmol.jpg")',
 				'line-v-gradient': 'linear-gradient(0deg, transparent 0%, var(--brand-gray-300) 40%, var(--brand-gray-300) 90%, transparent 100%)',
 				'line-h-gradient': 'linear-gradient(90deg, transparent 0%, var(--brand-gray-400) 40%, var(--brand-gray-400) 90%, transparent 100%)'
 			},
